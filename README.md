bf072746daee96f05a7d2069575544af4039ca7d
Evan Lucas <evanlucas@me.com>
2015-06-04 16:19:25 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# dtrace scripts for node/io.js
+
+Very basic for now
+
+A lot of help was found around GitHub.
+
+[https://github.com/joyent/nhttpsnoop](https://github.com/joyent/nhttpsnoop)
+was very useful.
---
+++ http-server-request.d
@@ -0,0 +1,23 @@
+#!/usr/bin/env dtrace -Cs
+
+#pragma D option quiet
+
+dtrace:::BEGIN
+{
+  printf("__________________________________________\n");
+  printf("| %6s | %-6s | %-20s |\n", "PID", "METHOD", "PATH");
+  printf("|--------|--------|----------------------|\n");
+}
+
+node*:::http-server-request
+{
+  printf("| %6d | %-6s | %-20s |\n",
+         pid,
+         copyinstr(arg4),
+         copyinstr(arg5));
+}
+
+dtrace:::END
+{
+  printf("done...\n");
+}
